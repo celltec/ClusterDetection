3230aff46e1404a908802cc73f87cba5368ad575
Jeremy Peters <contact@celltec.dev>
2022-02-06 17:42:10 +0100

    Add make_matrix

--- rust_solution/src/main.rs
+++ rust_solution/src/main.rs
@@ -1,3 +1,17 @@
+use rand::thread_rng;
+use rand::seq::SliceRandom;
+use std::time::Instant;
+
+const SIZE: usize = 10;
+
 fn main() {
-    println!("Hello, world!");
+    let start = Instant::now();
+    println!("{:?}", make_matrix());
+    let duration = start.elapsed();
+    println!("TIME: {:?}", duration);
+}
+
+fn make_matrix() -> [char; SIZE*SIZE] {
+    let mut rng = thread_rng();
+    [' '; SIZE*SIZE].map(|_| *['X', 'O'].choose(&mut rng).unwrap())
 }
