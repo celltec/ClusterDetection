4e20c983e1b983908f2ba2c575fec7a5fd99c98a
Jeremy Peters <contact@celltec.dev>
2022-02-27 16:22:06 +0100

    Add Square

--- rust_solution/src/lib.rs
+++ rust_solution/src/lib.rs
@@ -1,12 +1,12 @@
 #[macro_export]
 macro_rules! bench {
-    ($func: expr) => {
+    ($func: expr, $param: expr) => {
         let mut times: Vec<std::time::Duration> = vec!();
         for _ in 0..1000 {
             let start = std::time::Instant::now();
-            $func();
+            $func($param);
             times.push(start.elapsed());
         }
-        println!("{}: {:#?}", stringify!($func), times.iter().sum::<std::time::Duration>() / times.len() as u32);
+        println!("{}: {:?}", stringify!($func), times.iter().sum::<std::time::Duration>() / times.len() as u32);
     }
 }
--- rust_solution/src/main.rs
+++ rust_solution/src/main.rs
@@ -1,36 +1,53 @@
-use rand::thread_rng;
-use rand::seq::SliceRandom;
 use clap::{App, Arg};
-
 mod lib;
-
-const SIZE: usize = 10;
 
 fn main() {
     let args = App::new("ClusterDetection")
                     .about("Find the largest square cluster in a matrix")
-                    .arg(Arg::new("bench"))
+                    .arg(Arg::new("bench").short('b').long("bench"))
+                    .arg(Arg::new("size").default_value("10"))
                     .get_matches();
 
+    let size: u32 = std::cmp::max(args.value_of("size").unwrap().parse::<i32>().unwrap_or(10), 0) as u32;
+
     if args.is_present("bench") {
-        bench!(make_matrix);
+        println!("Bench a {}x{} matrix", size, size);
+        bench!(make_matrix, size);
+        bench!(get_clusters, make_matrix(size));
         std::process::exit(0);
     }
 
-    run();
+    run(size);
 }
 
-fn run() {
-    let matrix = make_matrix();
-    get_clusters(matrix);
-
+#[derive(Debug)]
+struct Square {
+    x: u32,
+    y: u32,
+    size: u32,
 }
 
-fn make_matrix() -> [char; SIZE*SIZE] {
-    let mut rng = thread_rng();
-    [' '; SIZE*SIZE].map(|_| *['X', 'O'].choose(&mut rng).unwrap())
+impl Square {
+    fn new(x: u32, y: u32, size: u32) -> Self {
+        Square { x, y, size }
+    }
+
+    fn print_info(&self) {
+        println!("{} {} {}", self.x, self.y, self.size);
+    }
 }
 
-fn get_clusters(matrix: [char; SIZE*SIZE]) {
+fn run(size: u32) {
+    println!("Find clusters in a {0}x{0} matrix:", size);
+    get_clusters(make_matrix(size));
+}
+
+fn make_matrix(size: u32) -> String {
+    random_string::generate((size*size) as usize, "XO")
+}
+
+fn get_clusters(matrix: String) {
+    let m1 = Square::new(1,2,4);
+    m1.print_info();
     println!("{:?}", matrix);
 }
