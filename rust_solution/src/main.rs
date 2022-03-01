b3a981be1e8034992f90a6ab959ce933be9333a7
Jeremy Peters <contact@celltec.dev>
2022-03-01 10:01:21 +0100

    BACKUP

--- rust_solution/src/main.rs
+++ rust_solution/src/main.rs
@@ -1,6 +1,24 @@
 use clap::{App, Arg};
 mod lib;
 
+struct Matrix {
+    size: u32,
+    data: String,
+}
+
+// struct Square {
+//     x: u32,
+//     y: u32,
+//     size: u32,
+// }
+
+// impl Square {
+//     // Optional
+//     fn new(x: u32, y: u32, size: u32) -> Self {
+//         Self { x, y, size }
+//     }
+// }
+
 fn main() {
     let args = App::new("ClusterDetection")
                     .about("Find the largest square cluster in a matrix")
@@ -13,41 +31,52 @@ fn main() {
     if args.is_present("bench") {
         println!("Bench a {}x{} matrix", size, size);
         bench!(make_matrix, size);
-        bench!(get_clusters, make_matrix(size));
+        bench!(get_clusters, &make_matrix(size));
         std::process::exit(0);
     }
 
     run(size);
 }
 
-#[derive(Debug)]
-struct Square {
-    x: u32,
-    y: u32,
-    size: u32,
-}
-
-impl Square {
-    fn new(x: u32, y: u32, size: u32) -> Self {
-        Square { x, y, size }
-    }
-
-    fn print_info(&self) {
-        println!("{} {} {}", self.x, self.y, self.size);
-    }
-}
-
 fn run(size: u32) {
     println!("Find clusters in a {0}x{0} matrix:", size);
-    get_clusters(make_matrix(size));
+    let matrix = make_matrix(size);
+    get_clusters(&matrix);
+    print_matrix(&matrix);
 }
 
-fn make_matrix(size: u32) -> String {
-    random_string::generate((size*size) as usize, "XO")
+fn make_matrix(size: u32) -> Matrix {
+    Matrix { size, data: random_string::generate((size*size) as usize, "XXO") }
 }
 
-fn get_clusters(matrix: String) {
-    let m1 = Square::new(1,2,4);
-    m1.print_info();
-    println!("{:?}", matrix);
+fn get_clusters(matrix: &Matrix) {//-> Vec<Square> {
+    let mut assumed_size = matrix.size as usize;
+
+    // for (i, c) in matrix.data.chars().enumerate() {
+    //     if i as u32 % matrix.size == 0 {
+    //         println!();
+    //     }
+    //     print!("{} ", c);
+    // }
+
+    while assumed_size > 1 {
+
+        let pos = matrix.data.find("X".repeat(assumed_size).as_str()).unwrap();
+        matrix.data.
+
+
+        assumed_size -= 1;
+    }
+
+    //Vec::new()
+}
+
+fn print_matrix(matrix: &Matrix) {
+    for (i, c) in matrix.data.chars().enumerate() {
+        if i as u32 % matrix.size == 0 {
+            println!();
+        }
+        print!("{} ", c);
+    }
+    println!();
 }
